2acd36ada21256d889670d139ed2597c4eb5a5be
danargolo <dan.argolo@gmail.com>
2024-11-21 21:20:14 -0300

    feat(front): implements loader

---
+++ front/src/components/Loader/index.tsx
@@ -0,0 +1,8 @@
+import './styles.css'
+
+export const Loader = () => (
+  <div className="loader">
+    <div className="dbl-spinner sp1"></div>
+    <div className="dbl-spinner dbl-spinner--2"></div>
+  </div>
+);
