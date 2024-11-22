3ec2929e3c3314773699a858b79ee2c1edc59910
danargolo <dan.argolo@gmail.com>
2024-11-22 09:43:55 -0300

    style: fix loader animation

--- front/src/components/Loader/index.tsx
+++ front/src/components/Loader/index.tsx
@@ -2,7 +2,7 @@ import './styles.css'
 
 export const Loader = () => (
   <div className="loader">
-    <div className="dbl-spinner sp1"></div>
+    <div className="dbl-spinner"></div>
     <div className="dbl-spinner dbl-spinner--2"></div>
   </div>
 );
