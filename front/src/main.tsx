0b5f6a2f01b21e58979bcb417f685fe5f46df2a0
danargolo <dan.argolo@gmail.com>
2024-11-27 20:49:20 -0300

    refactor: remove browserRouter

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,14 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 import { RenderProvider } from './context/renderContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <RenderProvider>
-      <App />
-    </RenderProvider>
-  </BrowserRouter>
+  <RenderProvider>
+    <App />
+  </RenderProvider>
+
   
 )
